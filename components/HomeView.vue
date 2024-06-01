7a98dc9ee1a523df83254d89c0fd0b707bbd3109
Quentin Juarez <quentin.juarez@hotmail.fr>
2024-06-02 01:34:28 +0200

    wording

--- components/HomeView.vue
+++ components/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex w-full flex-col items-center justify-center">
+  <div class="flex h-full w-full flex-col items-center justify-center">
     <h1 class="text-4xl font-bold">Welcome to the Home Page</h1>
-    <p class="mt-4 text-lg">This is a protected route</p>
+    <p class="mt-4 text-lg">Start by choosing a channel !</p>
   </div>
 </template>
 
