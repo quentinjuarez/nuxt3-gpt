13b412f0fe1a1f41112d30371e655e44faac8fc1
Quentin Juarez <quentin.juarez@hotmail.fr>
2024-06-02 15:09:32 +0200

    middlewares message

--- middleware/admin.ts
+++ middleware/admin.ts
@@ -2,6 +2,12 @@ export default defineNuxtRouteMiddleware((to, from) => {
   const store = useStore()
 
   if (!store.user?.isAdmin) {
+    errorToast({
+      statusMessage: 'Forbidden',
+      data: {
+        message: 'You are not authorized to access this page'
+      }
+    })
     return navigateTo('/')
   }
 
--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,6 +2,13 @@ export default defineNuxtRouteMiddleware((to, from) => {
   const store = useStore()
 
   if (!store.isAuthenticated) {
+    errorToast({
+      statusMessage: 'Unauthorized',
+      data: {
+        message: 'You need to login to access this page'
+      }
+    })
+
     const next = to.fullPath ? `?next=${to.fullPath}` : ''
     return navigateTo('/login' + next)
   }
